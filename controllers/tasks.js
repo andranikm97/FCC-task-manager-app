c94f3459caf7bdb823bec6849ee14c3ba552cdff
Andranik Markaryan <andranik.mrk@gmail.com>
2021-10-18 14:05:14 +0300

    Implement controllers' async wrapper
    Refactored code to include an asynchrounous wrapper function to wrap route controllers to avoid repetitive code in controllers. Although not the best change (let alone an understandable change), this, apparently, is the way to go.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,87 +1,64 @@
 const Task = require('../models/task');
-const getAllTasks = async (req, res) => {
-  try {
-    const existingTasks = await Task.find({});
-    // res.status(200).json({ existingTasks });
-    res.status(200).json({ existingTasks, amount: existingTasks.length });
-  } catch (error) {
-    res.status(500).json({ Message: e });
+const asyncWrapper = require('../middleware/asyncWrapper');
+
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const existingTasks = await Task.find({});
+  res.status(200).json({ existingTasks, amount: existingTasks.length });
+});
+
+const deleteAllTasks = asyncWrapper(async (req, res) => {
+  const deletedTasks = await Task.deleteMany({});
+  res.status(200).json({ deletedTasks });
+});
+
+const postTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
+
+const getTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+
+  if (!task) {
+    return res
+      .status(404)
+      .json({ Message: `The requested ID (${taskID}) was not found.` });
   }
-};
 
-const deleteAllTasks = async (req, res) => {
-  try {
-    const deletedTasks = await Task.deleteMany({});
-    res.status(200).json({ deletedTasks });
-  } catch (error) {
-    res.status(500).send(error);
+  res.status(200).json({ task });
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+
+  if (!task) {
+    return res
+      .status(404)
+      .json({ Message: `The requested ID (${taskID}) was not found.` });
   }
-};
 
-const postTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (e) {
-    res.status(500).json({ Message: e });
+  res.status(200).json({ task });
+});
+
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true, // return the new value
+    runValidators: true, // run validators, check conditions on data types
+    useFindAndModify: true,
+  });
+
+  if (!task) {
+    return res
+      .status(404)
+      .json({ Message: `The requested ID (${taskID}) was not found.` });
   }
-};
 
-const getTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOne({ _id: taskID });
-
-    if (!task) {
-      return res
-        .status(404)
-        .json({ Message: `The requested ID (${taskID}) was not found.` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ Message: error });
-  }
-};
-
-const deleteTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskID });
-
-    if (!task) {
-      return res
-        .status(404)
-        .json({ Message: `The requested ID (${taskID}) was not found.` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ Message: error });
-  }
-};
-
-const updateTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true, // return the new value
-      runValidators: true, // run validators, check conditions on data types
-      useFindAndModify: true,
-    });
-
-    if (!task) {
-      return res
-        .status(404)
-        .json({ Message: `The requested ID (${taskID}) was not found.` });
-    }
-
-    res.status(200).json({ id: taskID, task });
-  } catch (error) {
-    res.status(500).json({ Message: error });
-  }
-};
+  res.status(200).json({ id: taskID, task });
+});
 
 module.exports = {
   getAllTasks,
